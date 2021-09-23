cc18a730ab9585a753dbb3147c79fcabb461a288
Priyansh Patidar <patidar.priyansh936@gmail.com>
2021-09-24 00:48:17 +0530

    initial commit

---
+++ app.js
@@ -0,0 +1,26 @@
+const Twitter = require('twitter');
+const config = require('./config.js');
+
+const client = new Twitter(config);
+
+let params = {
+    q: '#30daysofgooglecloud -filter:retweets',
+    count: 10,
+    result_type: 'recent',
+    lang: 'en'
+};
+
+
+client.get('/search/tweets', params, (error, tweets, response) => {
+    if (!error) {
+        tweets.statuses.forEach((tweet) => {
+            client.post(`statuses/retweet/${tweet.id_str}`, (error, response) => {
+                if (!error) {
+                    console.log('Retweeted: ', tweet.id_str);
+                } else {
+                    console.log('Error: ', error);
+                }
+            });
+        });
+    }
+});
