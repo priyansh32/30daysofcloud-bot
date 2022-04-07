19ed52884814fc60dddb9eb47545fd1b9518b7bf
priyansh32 <patidar.priyansh936@gmail.com>
2022-04-07 12:19:49 +0530

    sss

--- index.js
+++ index.js
@@ -4,7 +4,7 @@ const config = require('./config.js');
 const client = new Twitter(config);
 
 let params = {
-    q: '@pikaso_me has:images',
+    q: 'from:pikaso_me is:reply',
     count: 50,
     result_type: 'recent',
     lang: 'en'
@@ -15,8 +15,9 @@ client.get('/search/tweets', params, (error, tweets, response) => {
     if (!error) {
         tweets.statuses.forEach((tweet) => {
             client.post('/statuses/update', {
-                status: `@pikaso_me ${tweet.text}`,
-                in_reply_to_status_id: tweet.id_str
+                status: `@pikaso_me screenshot this`,
+                in_reply_to_status_id: tweet.id_str,
+                auto_populate_reply_metadata: true
             }, (error, tweet, response) => {
                 if (!error) {
                     console.log(`${tweet.user.screen_name} replied to ${tweet.in_reply_to_screen_name}`);
