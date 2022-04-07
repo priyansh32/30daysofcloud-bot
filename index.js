d9c1543ff940bc051efd94084059c445c05d6ff9
Priyansh Patidar <patidar.priyansh936@gmail.com>
2022-04-07 12:11:02 +0530

    Update index.js

--- index.js
+++ index.js
@@ -4,7 +4,7 @@ const config = require('./config.js');
 const client = new Twitter(config);
 
 let params = {
-    q: '@pikaso_me has:images is:reply',
+    q: '@pikaso_me has:images',
     count: 50,
     result_type: 'recent',
     lang: 'en'
